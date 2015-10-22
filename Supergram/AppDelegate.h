2fca21621e500fc212c38c5d03da765bde8d84e2
Richard Martinez <shahasad78@gmail.com>
2015-10-22 14:40:14 -0700

    Initial Commit

---
+++ Supergram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Supergram
+//
+//  Created by Richard Martinez on 10/22/15.
+//  Copyright © 2015 Shotty Shack Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Supergram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Supergram
+//
+//  Created by Richard Martinez on 10/22/15.
+//  Copyright © 2015 Shotty Shack Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
